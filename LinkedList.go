30a32680c24eb5cdb9a1dafbf7ea3c06477b32da
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-15 13:43:18 +0545

    Initial Commit

---
+++ LinkedList.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LinkedList struct {
+	headNode *Node
+}
+
+type Error interface {
+	Error() string
+}
+
+func (linkedList *LinkedList) InsertAtHead(m int) {
+	node := Node{}
+	node.value = m
+	// if node.next != nil {
+	node.next = linkedList.headNode
+	// }
+	linkedList.headNode = &node
+}
+
+func (linkedList *LinkedList) IterateList() {
+	var node *Node
+	for node = linkedList.headNode; node != nil; node = node.next {
+		fmt.Printf("%d\t", node.value)
+	}
+}
+
+func (linkedList *LinkedList) LastNode() *Node {
+	var node *Node
+	var lastNode *Node
+	for node = linkedList.headNode; node != nil; node = node.next {
+		if node.next == nil {
+			lastNode = node
+		}
+	}
+	return lastNode
+}
+
+func (linkedList *LinkedList) InsertAtEnd(m int) {
+	node := &Node{m, nil}
+	var lastNode *Node
+	lastNode = linkedList.LastNode()
+	lastNode.next = node
+}
+
+func (linkedList *LinkedList) ReturnNode(m int) *Node {
+	var node *Node
+	for node = linkedList.headNode; node != nil; node = node.next {
+		if node.value == m {
+			return node
+		}
+	}
+	return node
+}
+
+func (linkedList *LinkedList) InsertAfter(nodeM int, m int) {
+	ourNode := &Node{m, nil}
+	requiredNode := linkedList.ReturnNode(nodeM)
+	if requiredNode.next != nil {
+		ourNode.next = requiredNode.next
+		requiredNode.next = ourNode
+	}
+}
+
+func main() {
+	linkedList := &LinkedList{}
+	linkedList.InsertAtHead(1)
+	linkedList.InsertAtHead(2)
+	linkedList.InsertAtHead(4)
+	linkedList.InsertAtEnd(5)
+	linkedList.IterateList()
+	linkedList.InsertAfter(2, 3)
+	fmt.Printf("\n")
+	linkedList.IterateList()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sahas001/test
+
+go 1.21.4
